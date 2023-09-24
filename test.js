1abe3068dd30b1eca6d0e4ba2b45f681f27bc226
harmox <tani_velikov@abv.bg>
2023-09-24 14:07:45 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,18 +1,36 @@
-function test(n) {
-    buff=``
-for(let i=0;i<n;i++){
-    letter=String.fromCharCode(97+i);
-    for(let b=0;b<n;b++){
-        better=String.fromCharCode(97+b);
-        for(let c=0;c<n;c++){
-            cetter=String.fromCharCode(97+c);
-            
-            buff+=letter
-            buff+=better
-            buff+=cetter+`\n`
-    
+function softuni(input) {
+    let change = Number(input[0])
+    let coins = Math.round(change * 100)
+    let totalCoinsCounter = 0;
+    while (coins > 0) {
+        if (coins >= 200) {
+            totalCoinsCounter++
+            coins -= 200
+        } else if (coins >= 100) {
+            totalCoinsCounter++
+            coins -= 100
+        } else if (coins >= 50) {
+            totalCoinsCounter++
+            coins -= 50
+        } else if (coins >= 20) {
+            totalCoinsCounter++
+            coins -= 20
+        } else if (coins >= 10) {
+            totalCoinsCounter++
+            coins -= 10
+        } else if (coins >= 5) {
+            totalCoinsCounter++
+            coins -= 5
         }
+        else if (coins >= 2) {
+            totalCoinsCounter++
+            coins -= 2
+        } else if (coins >= 1) {
+            totalCoinsCounter++
+            coins -= 1
+        }
+
     }
-}
-console.log(buff)
-} test(2)
+    console.log(totalCoinsCounter);
+
+} softuni(["0.59"])
